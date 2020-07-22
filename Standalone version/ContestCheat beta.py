5e7924916b1e4f05f31d66c944e8d7264dd03fc5
Ninnjah <45268461+Ninnjah@users.noreply.github.com>
2020-07-22 18:47:23 +0300

    Little FIXES and IMPROVMENT
    ADDED:
        ADDED version in title of main menu
    
    IMPROVEMENTS:
        IMPROVED Open website function
    
    
    FIXES:
        DELETED unnecessary function
        COMPETED comments

--- Standalone version/ContestCheat beta.py
+++ Standalone version/ContestCheat beta.py
@@ -6,6 +6,8 @@ import easyTui as tui                                   # Мой модуль д
 import os                                               # Для настройки консоли и работы с файлами
 import time                                             # Для установки задержек
 
+ver = '220720'
+
 os.system('mode con cols=64 lines=20')                  # Установка размеров консоли
 os.system('color 0A')                                   # Установка цвета консоли
 
@@ -15,14 +17,14 @@ def main(content):                                      #### Главное ме
     '''
     os.system('cls||clear')                             # Очистка консоли
                                                         ### Интерфейс
-    tui.title('Крутильщик классов для децкава конкурса')# Заголовок
+    tui.title('Крутильщик классов для децкава конкурса v.' + ver)# Заголовок
     tui.label('Что делать?')                            #
-    tui.ol(['Крутить классы', 
-            'Крутить классы нескольким участникам',     # Варианты ответов                
+    tui.ol(['Крутить классы',                           # Варианты ответов 
+            'Крутить классы нескольким участникам',     #                
             'Собрать новый список прокси',              #
             'Удалить список использованных прокси',     #
             'Вывести список участников',
-            'Открыть сайт конкурса'])                #
+            'Открыть сайт конкурса'])                   #
     print('чтобы вернуться сюда введи число меньше нуля')
     com = input()                                       ## Запрос команды пользователя
     try:                                                # проверка команды
@@ -62,37 +64,37 @@ def main(content):                                      #### Главное ме
         main(content)  
 
     elif com == 1:                                      ## Накрутка нескольким
-        def tryToCon(content):     
+        def tryToCon(content):                          
             os.system('cls||clear')                                 # Очистка консоли
             tui.title('Ща накрутим классов на конкурсе)')           # Заголовок
             tui.ul(['Чупа - 47703','Солнечный день - 47714'])       # Маркированный список
-            attempt = input('Сколько классов крутить?: ')       # Запрос команды на кол-во голосов
-            try:                                                # Проверка команды
+            attempt = input('Сколько классов крутить?: ')           # Запрос команды на кол-во голосов
+            try:                                                    ## Проверка команды
                 attempt = int(attempt)
             except:
                 print('нужно ввести цифру')
                 time.sleep(2)
                 tryToCon(content)                               
-            if attempt < 0:                                     # Выход в главное меню
+            if attempt < 0:                                         # Выход в главное маню
                 main(content)
             mem = input('за кого голосуем? ')                       # Запрос команды
-            if int(mem) < 0:
+            if int(mem) < 0:                                        # Проверка ввода
                 main(content)
-            mems = mem.split()
-            usedmem = []
-            for i in mems:
-                try:                                            # Попытка удалить
+            mems = mem.split()                                      # Преобразование ввода в список
+            usedmem = []                                            # Список участников за которых уже проголосовали
+            for i in mems:                                          # Берем поочередно каждого участника
+                try:                                                # Попытка удалить файл использованных прокси
                     os.remove('usedproxy.txt')
                     main(content)
-                except:                                         # Если неудача
-                    print('Список уже удален..')                # Вывод сообщения "список уже удален"
+                except:                                             # Если неудача
+                    print('Список уже удален..')                    # Вывод сообщения "список уже удален"
                     time.sleep(2)
-                usedmem.append(i)
+                usedmem.append(i)                                   # Записываем участника в использованные
                 url = 'https://stolicadetstva.com/competition/vote/' + i  # адресс голосования
-                os.system('cls||clear')                                 # Очистка консоли
-                tryToConnect(content, url, attempt)                     # Запуск функции накрутки
-        tryToCon(content)
-        main(content)    
+                os.system('cls||clear')                             # Очистка консоли
+                tryToConnect(content, url, attempt)                 # Запуск функции накрутки
+        tryToCon(content)                               # Запуск функции                                       
+        main(content)                                   # Выход в главное меню после выполнения
                     
     elif com == 2:                                      ## Сбор ip прокси из html файла
         proxyParser(content)
@@ -109,8 +111,21 @@ def main(content):                                      #### Главное ме
     elif com == 4:                                      ## Сбор информации об участниках
         membersFunc()
 
-    elif com == 5:
-        webbrowser.open('https://stolicadetstva.com/competition/work/277/', new=0, autoraise=True)
+    elif com == 5:                                      ## Открытие сайта конкурса
+        def openwebsite():
+            tui.ul(['"Мохнатые, пернатые" - 277'])                  # Пример ввода
+            contest = input('Введи номер конкурса: ')               # Запрос ввода
+            if contest.isnumeric() and len(contest):                # Проверка ввода
+                url = 'https://stolicadetstva.com/competition/work/' + contest
+                webbrowser.open(url, new=0, autoraise=True)
+            elif int(contest) < 0:                                  # Если ввод меньше 0, то переход в главное меню
+                main(content)                                       #
+            else:                                                   #
+                print('Нужно ввести числовой номер конкурса из 3 цифр..')
+                time.sleep(3)                                           #
+                openwebsite()
+        openwebsite()
+        main(content)
 
     else:                                               ## Проверка команды
         print('Нужно ввести цифру от 0 до 5')
@@ -121,29 +136,25 @@ def tryToConnect(content, url, attempt):                #### Накрутка г
     '''
     Подбор прокси из списка и попытка соединения с сервером
     '''
-    try:                                                # Попытка открыть и прочитать файл
-        with open('usedproxy.txt') as f:
-            usedproxy = f.readlines()
-        usedproxy = [x.strip() for x in usedproxy] 
+    try:                                                # Попытка открыть и прочитать файл с использованными прокси
+        with open('usedproxy.txt') as f:                # Открываем файл
+            usedproxy = f.readlines()                   # Записываем каждую строку файла в список
+        usedproxy = [x.strip() for x in usedproxy]      # Удаляем служебные символы
     except:                                             # При неудачи создать файл и открыть его
         usedproxy = open('usedproxy.txt', 'w')
         usedproxy.close()
-        with open('usedproxy.txt') as f:
-            usedproxy = f.readlines()
-        usedproxy = [x.strip() for x in usedproxy] 
-    if len(list(set(content) - set(usedproxy))) == 0:
-        tui.title('Неиспользованных прокси не осталось!')
-        tui.ul(['Зайдите в главное меню','Соберите новый список прокси [2]','Или удалите список использованных [3]\n\tэффективность будет хуже'])
-        input('\nНажмите "Enter"..')
-        main(content)
+        with open('usedproxy.txt') as f:                # Открываем файл
+            usedproxy = f.readlines()                   # Записываем каждую строку файла в список
+        usedproxy = [x.strip() for x in usedproxy]      # Удаляем служебные символы
+
+    ### Выводим сколько осталось неисползованных прокси
     print('Осталось неиспользованных прокси серверов :', len(list(set(content) - set(usedproxy))))
     usedproxyfile = open('usedproxy.txt', 'a')          # Открыть файл использованных прокси на дозапись
-    while attempt > 0:                                  # Пока попытки больше 0
-        for i in content:                               # берем каждый ip из файла
-            if i not in usedproxy:                      # И если он еще не использовался
-                ip = i
-                usedproxy.append(i)                     # Дописываем в список ip прокси
-                print(i, file=usedproxyfile)            # Записываем его в файл
+    while attempt > 0:                                  # Цикл накрутки
+        for ip in content:                              # Берем каждый ip из файла прокси
+            if ip not in usedproxy:                     # И если он еще не использовался
+                usedproxy.append(ip)                     # Дописываем в список ip прокси
+                print(ip, file=usedproxyfile)           # Записываем его в файл
                 usedproxyfile.close()                   # Закрываем файл
                 print(ip, end='\n')                     # Выводим на экран ip Прокси
                 try:                                    ## Попытка установить соединение
@@ -158,10 +169,11 @@ def tryToConnect(content, url, attempt):                #### Накрутка г
                     print('Not requesting')             # Выводим 'Not requesting'
                     time.sleep(1)                       # Ждем 1 секунду
                     tryToConnect(content, url, attempt) # Перезапускаем функцию
-            elif len(list(set(content + usedproxy))) == 0:
-                tui.title('Все прокси использованны..')
-                tui.ul(['Перейдите в главное меню','Соберите новый список прокси','Или очистите список использованных прокси'])
-                time.sleep(4)                           ## Главное меню
+            elif len(list(set(content) - set(usedproxy))) == 0:  ## Проверка на наличие неиспользованных прокси
+                tui.title('Неиспользованных прокси не осталось!')### Выводим информацию об этом и инструкцию по исправлению проблемы
+                tui.ul(['Зайдите в главное меню','Соберите новый список прокси [2]','Или удалите список использованных [3]\n\tэффективность будет хуже']) 
+                input('\nНажмите "Enter"..')                    # Ожидание пользователя
+                main(content)                                   # Выход в главное меню
 
 def proxyParser(content):                               #### Сбор списка прокси
     '''
@@ -209,18 +221,18 @@ def membersFunc():                                      #### Сбор инфор
     members = 0                                                 # Кол-во участников
     allready = []                                               # Список использованных имен
     membersDict = {}                                            # Словарь участников и кол-ва их голосов
-    tui.ul(['"Мохнатые, пернатые" - 277'])                      #
-    contest = input('Введи номер конкурса: ')                   #
-    if contest.isnumeric() and len(contest):                    #
+    tui.ul(['"Мохнатые, пернатые" - 277'])                      # Пример ввода
+    contest = input('Введи номер конкурса: ')                   # Запрос ввода
+    if contest.isnumeric() and len(contest):                    # Проверка ввода
         url = 'https://stolicadetstva.com/competition/work/' + contest
-    elif int(contest) < 0:                                      #
+    elif int(contest) < 0:                                      # Если ввод меньше 0 то выходим в главное меню
         main(content)                                           #
-    else:                                                       #
+    else:                                                       # Иначе сообщаем об ошибке ввода
         print('Нужно ввести числовой номер конкурса из 3 цифр..')
         time.sleep(3)                                           #
-        membersFunc()                                           #
-    os.system('cls||clear')                                     #
-    tui.title('Сбор информации об участниках')                  #
+        membersFunc()                                           # Перезапускаем функцию
+    os.system('cls||clear')                                     # Очищаем консоль
+    tui.title('Сбор информации об участниках')                  # Выводим заголовок
     r = requests.get(url)                                       # Запрос всей страницы
     soup = BeautifulSoup(r.text, 'html.parser')                 # Подготовка к парсингу
     lots = soup.find_all('li', 'compe_comment_li')              # Парсинг всех 'li' тегов с классом 'compe_comment_li'
@@ -228,37 +240,35 @@ def membersFunc():                                      #### Сбор инфор
 
     '''Парсинг имен и голосов'''
     for i in lot_ids:                                           # Преобразования тегов 'p' в список
-        lot_id.append(i.text)                                   #
+        lot_id.append(i.text)                                   # 
 
-
-    for i in lots:
-        for x in lot_id:
+    for i in lots:                                              # В каждом 'li' теге
+        for x in lot_id:                                        # Каждый 'p' тег
             if x in i.text and not x in allready:               # Поиск имен из lot_id в тегах 'li' не используемые ранее
                 ans = i.find('p', 'compe_comment').text         # Ищем текст тегов 'p' с классом 'compe_comment'
-                try:
-                    link = i.find(text='Голосую').parent.get('href')
-                    link = link.replace('/competition/vote/', '')
+                try:                                            # Проверяем есть ли в 'p'  теге строки 'Голосую'
+                    link = i.find(text='Голосую').parent.get('href') # и если есть берем ссылку
+                    link = link.replace('/competition/vote/', '') # Очищаем от лишнего
                     link = link.replace('/', '')
-                except:
+                except:                                         # Иначе пишем, что конкурс закончился
                     link = 'кончился'
                 ans = ans.replace(' | Голосую', '')             # Удаляем из текста ' | Голосую'
                 votes = int(ans.replace('Голосов: ', ''))       # Удаляем остальной текст для преобразования в int
                 allready.append(x)                              # Добавляем x в уже использованные
-                x = link + ': ' + x
+                x = link + ': ' + x                             # К каждому имени вначали приписываем Код голосования через двоеточие
                 membersDict.setdefault(x, votes)                # Создаем словарь из ключа x и значения votes
                 members += 1                                    # Увеличиваем кол-во участников
                 tui.updatingScore('Участников', members, 0)     # Запускаем счетчик участников
                 
-
     '''Сортировка участников по голосам и запись их в файл'''
     print('\n\n')
     votesList = list(membersDict.items())                       # Преобразование словаря участников в список
     votesList.sort(key=lambda i: i[1])                          # Сортировка списка по возрастанию значений словаря
-    membersTemp = members
+    membersTemp = members                                       # Создаем временнюю переменную участников
     for i in votesList:                                         # Вывод участников по возрастанию голосов
         print(membersTemp, '\t', i[0], ':', i[1])               #
         membersTemp -= 1                                        #
-    print('\n\nУчастников: ', members)                          #
+    print('\n\nУчастников: ', members)                          # Выводим общее кол-во участников
     ext = input('\nНажми "Enter", чтобы выйти в главное меню\nили введите "y" чтобы вывести список в файл\n')
     if ext == 'y':                                              # Запрос команды на печать списка в файл
         try:                                                    # Проверка файла на наличие
@@ -267,11 +277,11 @@ def membersFunc():                                      #### Сбор инфор
             f = open('members.txt', 'x')                        # Создаем файл и открываем на запись
         membersTemp = members                                   # Временная переменная для вывода топа
         for i in votesList:                                     # Запись участников в файл
-            print(membersTemp, '\t', i[0], ':', i[1], file=f)   #
+            print(membersTemp, '\t', i[0], ':', i[1], file=f)   # 
             membersTemp -= 1                                    #
         print('\n\nУчастников: ', members, file=f)              # Запись кол-ва участников в файл
         print('сохранено в файл "members.txt"')                 # 
-        f.close()                                               # Закрваем файл
+        f.close()                                               # Закрываем файл
         time.sleep(2)                                           # Ждем 2 секунды
     lot_id = None                                               # Список имен
     members = None                                              # Кол-во участников
